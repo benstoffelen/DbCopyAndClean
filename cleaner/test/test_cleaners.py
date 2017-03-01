d9f91eebd872002c718819a59269cfaccf1b185c
benstoffelen <ben.stoffelen@hm.edu>
2017-03-01 14:17:24 +0100

    created utilities class with helper function for cleaning strings

--- cleaner/test/test_cleaners.py
+++ cleaner/test/test_cleaners.py
@@ -1,18 +1,61 @@
-from unittest import TestCase
+# This Python file uses the following encoding: utf-8
+import unittest
+from cleaner.utilities import Utilities
 
-from cleaner.cleaners import Cleaners
 
-
-class TestCleaners(TestCase):
+class TestCleaners(unittest.TestCase):
     def test_clean_multiple_punctuations(self):
-        self.fail()
+        text1 = "schön zu sehen das sowas in coburg stattfindet...vielleicht besuche ich mal wieder die alte" + \
+                " heimat.. gut zu wissen ;)"
+        text2 = "Genau für Mich!!!!"
+        cleaner = Utilities()
+        result1 = "schön zu sehen das sowas in coburg stattfindet. vielleicht besuche ich mal wieder die alte" + \
+                  " heimat. gut zu wissen ;)"
+        result2 = "Genau für Mich!"
+        self.assertEqual(cleaner.clean_multiple_punctuations(text1), result1)
+        self.assertEqual(cleaner.clean_multiple_punctuations(text2), result2)
+
+    def test_clean_url(self):
+        text = "vom 13. bis 16. mai findet in Coburg das 34. BMW Veteranentreffen statt," + \
+               "Infos auch unter http://www.facebook.com/TourismusCoburg?v=app_2344061033&ref=ts#!/event.php" + \
+               "?eid=110512678986182&index=1"
+        cleaner = Utilities()
+        result = "vom 13. bis 16. mai findet in Coburg das 34. BMW Veteranentreffen statt,Infos auch unter"
+        self.assertEqual(cleaner.clean_url(text), result)
 
     def test_detect_language_langid(self):
-        cleaner = Cleaners()
+        cleaner = Utilities()
         with open("comments.txt") as f:
             for line in f:
                 line = line.replace('"', "")
                 lang = cleaner.check_language_langid(line)
                 print line
                 print lang
-        self.fail()
+        pass
+
+    @unittest.skip("testing skipping")
+    def test_detect_language_langdetect(self):
+        cleaner = Utilities()
+        with open("comments.txt") as f:
+            for line in f:
+                line = line.replace('"', "")
+                lang = cleaner.check_language_languagedetect(line)
+                print line
+                print lang
+        pass
+
+    def test_clean_smileys(self):
+        cleaner = Utilities()
+        test1 = ":o))"
+        test2 = "UUU Nice Lets Play Rock n Roll  :))"
+        self.assertEqual(cleaner.clean_smileys(test1), ":o)")
+        self.assertEqual(cleaner.clean_smileys(test2), "UUU Nice Lets Play Rock n Roll  :)")
+
+    def test_clean_multiple_whitespaces(self):
+        cleaner = Utilities()
+        test1 = "UUU Nice Lets Play Rock n Roll  :))"
+        self.assertEqual(cleaner.clean_double_whitespaces(test1), "UUU Nice Lets Play Rock n Roll :))")
+
+    def test_clean_beginning_punct(self):
+        cleaner = Utilities()
+        self.assertEqual("Sonnebrille und wech",cleaner.clean_dots_beginning_of_text("...Sonnebrille und wech"))
---
+++ cleaner/utilities.py
@@ -0,0 +1,46 @@
+import langdetect
+import re
+from langid.langid import LanguageIdentifier, model
+
+from cleaner import urlmarker
+
+
+class Utilities:
+    def __init__(self):
+        self._identifier = LanguageIdentifier.from_modelstring(model, norm_probs=True)
+
+    #Method to remove multiple punctuations. E.g. ..... --> .
+    @staticmethod
+    def clean_multiple_punctuations(text):
+        doubles_removed = re.sub(r'[.!?\-)/\\:\']+(?=[.!?\-)/\\:\'])', '', text)
+        return re.sub(r'([.?!])(\S)', r"\1 \2", doubles_removed)
+
+    #Method to detect text language using the langid framewok
+    def check_language_langid(self, text):
+        return self._identifier.classify(text)[0]
+
+    #Method to detect text language using the langdetect framework
+    @staticmethod
+    def check_language_languagedetect(text):
+        return langdetect.detect(text)
+
+    # Method to clean urls from text
+    @staticmethod
+    def clean_url(text):
+        return re.sub(urlmarker.WEB_URL_REGEX, '', text).rstrip()
+
+    # Method to reduce smileys to their general form. E.g. :-DDDDDDD --> :-D
+    # TODO BeSt: maybe there is a database that can do this out of the box?
+    @staticmethod
+    def clean_smileys(text):
+        return re.sub(r'([\[\]@{]?[:;8]+[o-]?)([D)XBbdSs"(O\)\[\\pP?])+(?<=[D)XBbdSs"(O\)\[\\pP?])', r"\1\2", text)
+
+    # Method to remove multiple whitespaces from text
+    @staticmethod
+    def clean_multiple_whitespaces(text):
+        return ' '.join(text.split())
+
+    #Method to remove ... from beginning of text
+    @staticmethod
+    def clean_dots_beginning_of_text(text):
+        return re.sub(r'^[.]+\s*(\w*)',r"\1", text)
